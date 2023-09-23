9525b834046bb13756b3d833abf96f5b8f55e622
ABDI TIRUNEH <abdi.tirunehh@gmail.com>
2023-09-23 11:28:58 +0300

    initial app setup

---
+++ src/main/java/com/bettingApp/BettingGameApplication.java
@@ -0,0 +1,12 @@
+package com.bettingApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BettingGameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BettingGameApplication.class, args);
+    }
+}
---
+++ src/main/resources/templates/email-template.ftl
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Welcome</title>
+        <style>
+            .textBlue{
+                color: "#00adef";
+            }
+        </style>
+    </head>
+    <body>
+        <p>Dear ${name},</p>
+        <p class="textBlue">${message}</p>
+        <p class="textBlue">${link}</p>
+    </body>
+</html>
